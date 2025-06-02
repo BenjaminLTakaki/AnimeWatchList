f0dd2d5b2082fbe7fbdb2a436e048eba7655c4a2
BenjaminLTakaki <b.takaki@student.fontys.nl>
2025-06-02 16:54:20 +0200

    Fix import statement for models to use relative path

--- projects/skillstown/app.py
+++ projects/skillstown/app.py
@@ -31,7 +31,7 @@ def get_url_for(*args, **kwargs):
     return url
 
 # Import models after db is defined
-from models import Company, Student, Category, ContentPage, Course, CourseContentPage, UserProfile, SkillsTownCourse, CourseDetail
+from .models import Company, Student, Category, ContentPage, Course, CourseContentPage, UserProfile, SkillsTownCourse, CourseDetail
 
 # Auth setup
 def init_auth(app, get_url_for_func, get_stats_func):
